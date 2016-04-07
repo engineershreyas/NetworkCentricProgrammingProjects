418c85478770486c529ce02728fc1f393245d6ac
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-04-07 16:20:43 -0400

    for priyesh

--- ProjectFour/ssh90-tftp/server.c
+++ ProjectFour/ssh90-tftp/server.c
@@ -55,6 +55,7 @@ void do_stuff(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen, int port)
     uint16_t b_num;
     len = clilen;
     n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+    printf("mesg = %s",mesg);
     if(get_opcode(mesg,n,&opcode) == 0){
       char filename[MAXLINE];
       int f = 0;
