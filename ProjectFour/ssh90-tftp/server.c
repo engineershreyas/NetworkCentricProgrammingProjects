554338b5380d340221636c0d4f8cea2774271c39
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-04-10 19:55:27 -0400

    hehe

--- ProjectFour/ssh90-tftp/server.c
+++ ProjectFour/ssh90-tftp/server.c
@@ -58,7 +58,8 @@ void do_stuff(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen, int port)
     len = clilen;
     n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
     printf("n = %d",n);
-    if(n != -1 || get_opcode(mesg,n,&opcode) == 0){
+    if(n != -1){
+    if(get_opcode(mesg,n,&opcode) == 0){
       char filename[MAXLINE];
       int f = 0;
       char mode_str[100];
@@ -181,6 +182,7 @@ void do_stuff(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen, int port)
           m = 0;
 
 
+
       }
       else if(opcode == 3){
         //data
@@ -200,6 +202,8 @@ void do_stuff(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen, int port)
     else{
       printf("Error decoding opcode\n");
     }
+  }
+
 
   }
 }
